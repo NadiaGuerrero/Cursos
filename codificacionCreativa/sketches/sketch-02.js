c024eb580fd140fac2e5b27918bf24e5b581385c
NadiaGuerrero <guerrero.s.nadia@gmail.com>
2023-02-02 20:08:38 -0600

    Comienzo U2

--- codificacionCreativa/sketches/sketch-02.js
+++ codificacionCreativa/sketches/sketch-02.js
@@ -1,13 +1,25 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'turquoise';
+
+    const x = width * 0, y = height * 0;
+    const w = width * 0.5, h = height * 0.5;
+
+    context.beginPath();
+    context.rect(x,y,w,h);
+    context.fill();
+
+    // Transforming the context
+    context.rotate()
   };
 };
 
